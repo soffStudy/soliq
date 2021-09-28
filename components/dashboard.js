e4942ae0a9f16ee61c78b4c70bd68b7c12431f09
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-09-28 13:10:59 +0500

    bayonnoma va qaror yaxshilandi

--- components/dashboard.js
+++ components/dashboard.js
@@ -3,8 +3,7 @@ import Components from '../styleW/componentsW'
 import Link from 'next/link'
 import { AiOutlineUnorderedList, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { HiOutlineDocumentText } from "react-icons/hi";
-// import Slide from 'react-reveal/Slide';
-
+import Fade from 'react-reveal/Fade';
 
 
 
@@ -21,7 +20,7 @@ const Dashboard = () => {
             <div className="container locked">
                 <div className='dashboard'>
                     <div className='d-flex justify-content-center'>
-                        <img src="https://play-lh.googleusercontent.com/WsnTVMB-ichRSKymTZ13f8M_Q_4XlfgofdYk3V4JsFe7J4nxYZ8J_YUZQxmWjs4zsYIf=s1200" className={`${tugma ? "gerb" : "gerb2"}`} alt="" />
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Emblem_of_Uzbekistan.svg/200px-Emblem_of_Uzbekistan.svg.png" className={`${tugma ? "gerb" : "gerb2"}`} alt="" />
 
                     </div>
                     <div className='d-flex justify-content-center mt-2 mb-2'>
@@ -30,40 +29,57 @@ const Dashboard = () => {
                     </div>
                     <ul>
                         <li>
-                            <Link href="/menu">
-                                <a className='d-flex'>
-                                    <span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><AiOutlineHome />
-                                    </span>
-                                    <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
-                                        Menu
-                                    </span>
-                                </a>
-                            </Link>
+                            <Fade left >
+                                <Link href="/menu">
+                                    <a className='d-flex'>
+                                        <span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><AiOutlineHome />
+                                        </span>
+                                        <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
+                                            Menu
+                                        </span>
+                                    </a>
+                                </Link>
+                            </Fade>
+
+
+
                         </li>
                         <li>
-                            <Link href="/table">
-                                <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><AiOutlineUser /> </span>
-                                    <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
-                                        Users</span></a>
-                            </Link>
+                            <Fade left>
+                                <Link href="/table">
+                                    <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><AiOutlineUser /> </span>
+                                        <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
+                                            Users</span></a>
+                                </Link>
+                            </Fade>
+
                         </li>
                         <li>
-                            <Link href="twoCards">
-                                <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
-                                    Document</span></a>
-                            </Link>
+                            <Fade left>
+                                <Link href="twoCards">
+                                    <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
+                                        Document</span></a>
+                                </Link>
+                            </Fade>
+
                         </li>
                         <li>
-                            <Link href="bayonnoma">
-                                <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
-                                    Bayonnoma</span></a>
-                            </Link>
+                            <Fade left>
+                                <Link href="bayonnoma">
+                                    <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
+                                        Bayonnoma</span></a>
+                                </Link>
+                            </Fade>
+
                         </li>
                         <li>
-                            <Link href="qaror">
-                                <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
-                                    Qaror</span></a>
-                            </Link>
+                            <Fade left>
+                                <Link href="qaror">
+                                    <a className='d-flex'><span className={`${tugma ? "p-1 d-flex align-items-center" : "d-flex justify-contenr-center p-1 ms-1"}`}><HiOutlineDocumentText /> </span>  <span className={`${tugma ? "menuWord2" : "menuWord"}`}>
+                                        Qaror</span></a>
+                                </Link>
+                            </Fade>
+
                         </li>
                     </ul>
                 </div>
